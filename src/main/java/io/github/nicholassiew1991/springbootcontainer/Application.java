0e7496ee8edf0d0a2655c1755c97377751ca628d
Nicholas Siew <nicholas.siewkl1991@gmail.com>
2023-05-21 14:20:24 +0800

    Initial commit

---
+++ src/main/java/io/github/nicholassiew1991/springbootcontainer/Application.java
@@ -0,0 +1,13 @@
+package io.github.nicholassiew1991.springbootcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
